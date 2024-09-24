ccdb69147101a67d09c8d725f79e57c89b8149fb
David Brownhill <davidbrownhill70@btinternet.com>
2024-09-24 23:41:13 +0100

    Second Commit.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,28 @@
 package org.example;
 
-/**
- * Hello world!
- */
+import java.util.*;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    String LongestWord(String sen) {
+        String[] senArray = sen.split("[^a-z^0-9]");
+
+        int longestIndex = 0;
+        String longestWord = "";
+
+        for (int i = 0; i < senArray.length; i++) {
+
+            if (senArray[i].length() > longestIndex) {
+                longestWord = senArray[i];
+                longestIndex = senArray[i].length();
+            }
+        }
+        return longestWord;
+    }
+    public static void main (String[] args) {
+        // keep this function call here
+        Scanner  s = new Scanner(System.in);
+        App c = new App();
+        System.out.print(c.LongestWord(s.nextLine()));
+
     }
 }
